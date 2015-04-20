f3b879267d0164d07836c050845317746950a073
alexkey77 <alexander.key@mavs.uta.edu>
2015-04-20 18:33:08 -0500

    xml delete

--- gen/com/example/example/R.java
+++ gen/com/example/example/R.java
@@ -25,30 +25,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int FrameLayout2=0x7f080006;
-        public static final int RelativeLayout1=0x7f080002;
-        public static final int Space1=0x7f080003;
-        public static final int Space2=0x7f080005;
-        public static final int Space3=0x7f080004;
-        public static final int TextView01=0x7f080007;
-        public static final int TextView02=0x7f08000a;
-        public static final int action_settings=0x7f08000f;
-        public static final int buttonNavigateMainActivity=0x7f08000b;
-        public static final int buttonNavigateRegisterActivity=0x7f08000c;
-        public static final int buttonNavigateSecondActivity=0x7f080000;
-        public static final int editText1=0x7f080001;
-        public static final int editText2=0x7f080009;
-        public static final int textView1=0x7f080008;
-        public static final int textViewActivity2=0x7f08000d;
-        public static final int textViewSettings=0x7f08000e;
+        public static final int FrameLayout2=0x7f080004;
+        public static final int RelativeLayout1=0x7f080000;
+        public static final int Space1=0x7f080001;
+        public static final int Space2=0x7f080003;
+        public static final int Space3=0x7f080002;
+        public static final int TextView01=0x7f080005;
+        public static final int TextView02=0x7f080009;
+        public static final int action_settings=0x7f08000e;
+        public static final int buttonNavigateMainActivity=0x7f08000a;
+        public static final int buttonNavigateRegisterActivity=0x7f08000b;
+        public static final int editText1=0x7f080006;
+        public static final int editText2=0x7f080008;
+        public static final int textView1=0x7f080007;
+        public static final int textViewActivity2=0x7f08000c;
+        public static final int textViewSettings=0x7f08000d;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int login_activity=0x7f030001;
-        public static final int menu_activity=0x7f030002;
-        public static final int register_activity=0x7f030003;
-        public static final int second_activity=0x7f030004;
-        public static final int settings=0x7f030005;
+        public static final int login_activity=0x7f030000;
+        public static final int menu_activity=0x7f030001;
+        public static final int register_activity=0x7f030002;
+        public static final int settings=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
